2f11b1cec9a384b6003529a9dcc6870870d8712a
darkolorin <darkolorin@gmail.com>
2021-12-06 23:38:01 +0300

    next

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
+use std::io;
+
 fn main() {
-    let somevalue = 123;
-    println!("Hello, world! {}", somevalue);
-  }
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
+}
